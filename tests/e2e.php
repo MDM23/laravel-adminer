f8402234b6c843363c4fb872e3eba1da13c7c7ef
Peter Frank <mdm23@gmx.de>
2019-02-12 16:30:30 +0100

    Fix syntax error and Laravel master version reference

--- tests/e2e.php
+++ tests/e2e.php
@@ -62,7 +62,7 @@ EOT);
 
     if (!empty($evaluation->getReturnValue())) {
         echo "UNEXPECTED ERROR: " . $evaluation->getReturnValue() . PHP_EOL;
-        exit 1;
+        exit(1);
     }
 }
 
